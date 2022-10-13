a40a45bb11c7da4b474d60d35f348c8afffbcf8b
sunggookang <rkdtjdrn90@gmail.com>
2022-10-13 22:35:08 +0900

    First Commit

--- myboard/settings.py
+++ myboard/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
